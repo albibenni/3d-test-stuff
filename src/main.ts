1472ce9d34f23873afa27f22ad20fd4c1bf46a4c
Alberto Benatti <albi.benni8@gmail.com>
2024-08-07 21:44:19 +0200

    just bare webgl2

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 console.log("hello 3d");
+function init() {
+    const canvas = document.createElement("canvas");
+
+
+    const gl = canvas.getContext('webgl2');
+
+    const message = gl ? "we got webgl2" : "we dont";
+
+    console.log(message);
+}
+
+window.onload = init;
