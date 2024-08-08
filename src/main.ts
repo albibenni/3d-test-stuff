9f495defec9e1fbc17a78f1cc3cbd31de4b63c90
Alberto Benatti <albi.benni8@gmail.com>
2024-08-08 21:03:16 +0200

    played with clearcolor types and keyboard events

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 console.log("hello 3d");
 let gl: WebGL2RenderingContext;
 
-function updateClearColor(...color: any) {
+function updateClearColor(...color: number[]) {
     //@ts-ignore
     gl.clearColor(...color);
     gl.clear(gl.COLOR_BUFFER_BIT);
@@ -9,24 +9,24 @@ function updateClearColor(...color: any) {
 }
 
 function checkKey(event: KeyboardEvent) {
-    switch (event.keyCode) {
+    switch (event.key) {
         // number 1 => green
-        case 49: {
+        case "1": {
             updateClearColor(0.2, 0.8, 0.2, 1.0);
             break;
         }
         // number 2 => blue
-        case 50: {
+        case "2": {
             updateClearColor(0.2, 0.2, 0.8, 1.0);
             break;
         }
         // number 3 => random color
-        case 51: {
+        case "3": {
             updateClearColor(Math.random(), Math.random(), Math.random(), 1.0);
             break;
         }
-        // number 4 => get color
-        case 52: {
+        // number 0 => get color
+        case "0": {
             const color = gl.getParameter(gl.COLOR_CLEAR_VALUE);
             // Don't let the following line confuse you.
             // It basically rounds up the numbers to one
@@ -34,7 +34,9 @@ function checkKey(event: KeyboardEvent) {
             // TIP: Given that WebGL's color space ranges
             // from 0 to 1 you can multiply these values by 255
             // to display in their RGB values.
-            alert(`clearColor = (${color[0].toFixed(1)}, ${color[1].toFixed(1)}, ${color[2].toFixed(1)})`);
+            alert(
+                `clearColor = (${color[0].toFixed(1)}, ${color[1].toFixed(1)}, ${color[2].toFixed(1)})`,
+            );
             window.focus();
             break;
         }
@@ -50,7 +52,6 @@ function init() {
     } else {
         throw new Error("Mayday");
     }
-
 }
 
 window.onload = init;
